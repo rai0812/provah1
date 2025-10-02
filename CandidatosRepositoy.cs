54181b6247ab7c878e8f251f9ce022a7fb7ec6df
rai0812 <raissa14m@gmail.com>
2025-10-02 20:35:48 -0300

    Add files via upload

---
+++ CandidatosRepositoy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repositories
+{
+    public class CandidatosRepositoy : ICandidatosRepository
+
+    {
+        private readonly List<Candidatos> _candidatos = new();
+        public void Adicionar(Candidatos candidatos)
+        {
+            _candidatos.Add(candidatos);
+
+        }
+
+
+        public bool ExisteNumeoProjeto(int NumProjeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Candidatos> GetAll()
+        {
+            return _candidatos;
+        }
+
+    
+    }
+
+}
